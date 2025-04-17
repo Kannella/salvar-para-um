2f82323e569e7abadc51dcf7424c47fef87cef56
Giovanni <gi.canella85@outlook.com>
2025-04-17 00:51:13 -0300

    removendo todos os arquivos rastreados da branch main

--- src/main/java/com/devsuperior/aula/repositories/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.aula.repositories;
-
-import com.devsuperior.aula.entities.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- src/main/java/com/devsuperior/aula/repositories/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.aula.repositories;
-
-import com.devsuperior.aula.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
